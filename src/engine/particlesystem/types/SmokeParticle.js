ffc95a363c7f5528a2d001b180a20585c1dcf649
bfattori <bfattori@gmail.com>
2026-08-25 19:42:28 -0400

    Documentation additions

--- src/engine/particlesystem/types/SmokeParticle.js
+++ src/engine/particlesystem/types/SmokeParticle.js
@@ -11,11 +11,27 @@ export default class SmokeParticle extends PhysicalParticle {
             dragRate: 0,
             particleSize: [10, 16],
             velocity: 0.008,
-            curl: [0.0003, 0.002],
-            curlIntensity: 0.8,
             growth: 0.1,
+            gravity: [0.0, -0.004],
             softness: 0.15,
-            gravity: [0.0, -0.004]
+
+            /**
+             * The amount of curl applied to each particle throughout its life
+             * @type {number|Array<number>}
+             */
+            curl: [0.0003, 0.002],
+
+            /**
+             * The intensity of the curl over 1 second. Lower values result in less curling.
+             * @type {number|Array<number>}
+             */
+            curlIntensity: 0.8,
+            
+            /**
+             * The range of curl variance applied to the particle
+             * @type {number|Array<number>}
+             */
+            curlVariance: 0.53,
         }, url);
         this.merge(opts);
         this.name = 'smokeParticle';
@@ -34,7 +50,7 @@ export default class SmokeParticle extends PhysicalParticle {
     spawn(pEngine, time, config) {
         const particle = super.spawn(pEngine, time, config);
         particle.memory.curl = $Math.randomRange(this.curl[0], this.curl[1]);
-        particle.memory.dir = Util.selectRandom(-0.53, 0.53);
+        particle.memory.cD = Util.selectRandom(-this.curlVariance, this.curlVariance);
         particle.memory.cI = this.curlIntensity * 1000;
         return particle
     }
@@ -53,7 +69,7 @@ export default class SmokeParticle extends PhysicalParticle {
         super.update(pEngine, time, deltaTime, $memory, pos, vel, life);
 
         // apply curl
-        vel[0] += ($memory.curl * Math.cos(($memory.ttl - life) / $memory.cI)) * $memory.dir;
+        vel[0] += ($memory.curl * Math.cos(($memory.ttl - life) / $memory.cI)) * $memory.cD;
         vel[1] += $memory.curl * Math.sin(($memory.ttl - life) / $memory.cI);
     }
 }
